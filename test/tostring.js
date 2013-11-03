311362ce8a56dc7528d0099e0eb2db5c8c02866c
Jérémy Lal <kapouer@melix.org>
2013-11-03 02:08:37 +0100

    Test uppercase output

--- test/tostring.js
+++ test/tostring.js
@@ -1,31 +1,32 @@
 
 var bytes = require('..')
+  , tb = 1 << 40
   , gb = 1 << 30
   , mb = 1 << 20
   , kb = 1 << 10;
 
 describe('bytes(number)', function () {
   it('should convert numbers < 1024 to `bytes` string', function () {
-    bytes(200).should.eql('200b');
+    bytes(200).should.eql('200B');
   })
 
-  it('should convert numbers >= 1024 to kb string', function () {
-    bytes(kb).should.equal('1kb')
-    bytes(2 * kb).should.equal('2kb')
+  it('should convert numbers >= 1024 to KB string', function () {
+    bytes(kb).should.equal('1KB')
+    bytes(2 * kb).should.equal('2KB')
   })
 
-  it('should convert numbers >= 1048576 to mb string', function () {
-    bytes(mb).should.equal('1mb')
-    bytes(2 * mb).should.equal('2mb')
+  it('should convert numbers >= 1048576 to MB string', function () {
+    bytes(mb).should.equal('1MB')
+    bytes(2 * mb).should.equal('2MB')
   })
 
-  it('should convert numbers >= (1 << 30) to gb string', function () {
-    bytes(gb).should.equal('1gb')
-    bytes(2 * gb).should.equal('2gb')
+  it('should convert numbers >= (1 << 30) to GB string', function () {
+    bytes(gb).should.equal('1GB')
+    bytes(2 * gb).should.equal('2GB')
   })
 
   it('should support floats', function () {
-    bytes(1.2 * mb).should.equal('1.2mb')
-    bytes(1.2 * kb).should.equal('1.2kb')
+    bytes(1.2 * mb).should.equal('1.2MB')
+    bytes(1.2 * kb).should.equal('1.2KB')
   })
 })
