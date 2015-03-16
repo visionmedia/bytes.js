aeae258e39fc8219bcd7d9b27e2b2e0fcbd66b3f
Théo FIDRY <theo.fidry@gmail.com>
2015-03-16 16:27:42 +0100

    Adopted singleton pattern.

--- index.js
+++ index.js
@@ -13,48 +13,34 @@ var convertFunction = require(__dirname + '/lib/byte-convert.js'),
  *  }} [options] Parser options.
  *
  * @returns {string|number|null}
+ * @constructor
+ */
+function Parser() {}
+
+/**
+ * Convert the given value in bytes into a string.
+ *
+ * If the value is negative, it is kept as such. If it is a float, it is rounded.
+ *
+ * @param {number} value Value to convert
+ * @param {{
+ *  case: ?string=,
+ *  thousandSeparator: ?string=
+ * }} [options] See byte parser options.
+ *
+ * @return {string}
  * @api public
  */
-function parser(value, options) {
+Parser.prototype.convert = convertFunction;
 
-  if (this) {
-    // Case where parser is called as a constructor
-    /**
-     * Convert the given value in bytes into a string.
-     *
-     * If the value is negative, it is kept as such. If it is a float, it is rounded.
-     *
-     * @param {number} value Value to convert
-     * @param {{
-     *  case: ?string=,
-     *  thousandSeparator: ?string=
-     * }} [options] See byte parser options.
-     *
-     * @return {string}
-     * @api public
-     */
-    this.convert = convertFunction;
+/**
+ * Parse the string value into an integer in bytes. If no unit is given, it is assumed the value is in bytes.
+ *
+ * @param {number} value
+ *
+ * @returns {number|null}
+ * @api public
+ */
+Parser.prototype.parse = parseFunction;
 
-    /**
-     * Parse the string value into an integer in bytes. If no unit is given, it is assumed the value is in bytes.
-     *
-     * @param {number} value
-     *
-     * @returns {number|null}
-     * @api public
-     */
-    this.parse = parseFunction;
-  }
-
-  if (typeof value === 'string') {
-    return parseFunction(value, options);
-  }
-
-  if (typeof value === 'number') {
-    return convertFunction(value);
-  }
-
-  return null;
-}
-
-module.exports = parser;
+module.exports = new Parser();
--- test/bytes.js
+++ test/bytes.js
@@ -1,46 +1,21 @@
 'use strict';
 
-var bytesParser = require(__dirname + '/../index.js'),
+var BytesParser = require(__dirname + '/../index.js'),
     expect = require('chai').expect;
 
 describe('Test constructor', function() {
 
-  describe('Should be usable as a function', function() {
-
-    it('Should return null if input invalid', function() {
-      var inputs = [undefined, null, false, true, function() {}, {}];
-      for (var k in inputs) {
-        expect(bytesParser(inputs[k])).be.null;
-      }
-    });
-
-    it('Should parse a string into a number', function() {
-      // This function is testes more accuratly in another test suite
-      expect(bytesParser('1Kb')).to.equal(1024);
-    });
-
-    it('Should convert a number into a string', function() {
-      // This function is testes more accuratly in another test suite
-      expect(bytesParser(1024)).to.equal('1Kb');
-    });
+  it('Shoud not be instantiable', function() {
+    expect(function() {new BytesParser();}).to.throw();
   });
 
-  describe('Should be usable as a class', function() {
+  it('Shoud be able to parse a string into a number', function() {
+    // This function is testes more accuratly in another test suite
+    expect(BytesParser.parse('1Kb')).to.equal(1024);
+  });
 
-    it('Shoud be instantiable', function() {
-      var instance = new bytesParser();
-    });
-
-    it('Shoud be able to parse a string into a number', function() {
-      // This function is testes more accuratly in another test suite
-      var instance = new bytesParser();
-      expect(instance.parse('1Kb')).to.equal(1024);
-    });
-
-    it('Should convert a number into a string', function() {
-      // This function is testes more accuratly in another test suite
-      var instance = new bytesParser();
-      expect(instance.convert(1024)).to.equal('1Kb');
-    });
+  it('Should convert a number into a string', function() {
+    // This function is testes more accuratly in another test suite
+    expect(BytesParser.convert(1024)).to.equal('1Kb');
   });
 });
