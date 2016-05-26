9892b14a183acebc49bd9511ced796975b20cbb2
Guillaume Legrain <legrainguillaume@gmail.com>
2016-05-26 18:57:40 +0200

    moved spaceBeforeUnit test into individual test

--- test/bytes.js
+++ test/bytes.js
@@ -21,6 +21,9 @@ describe('Test constructor', function(){
   it('Should convert a number into a string with options', function(){
     // This function is testes more accurately in another test suite
     assert.equal(bytes(1000, {thousandsSeparator: ' '}), '1 000B');
+  });
+
+  it('Should insert a space between number and unit', function(){
     // Test space before unit option
     assert.equal(bytes(1024, {spaceBeforeUnit: true}), '1 kB');
   });
