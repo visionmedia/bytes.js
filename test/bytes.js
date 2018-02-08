53a957b51cb95d54287e06b6684bf00f24ab2295
believer <rickard.laurin@gmail.com>
2018-02-08 20:00:54 +0100

    Fix typos in tests

--- test/bytes.js
+++ test/bytes.js
@@ -3,33 +3,33 @@
 var assert = require('assert');
 var bytes = require('../index.js');
 
-describe('Test constructor', function(){
-  it('Expect a function', function(){
+describe('Test constructor', function () {
+  it('Expect a function', function () {
     assert.equal(typeof bytes, 'function');
   });
 
-  it('Should return null if input invalid', function(){
+  it('Should return null if input is invalid', function () {
     assert.strictEqual(bytes(undefined), null);
     assert.strictEqual(bytes(null), null);
     assert.strictEqual(bytes(true), null);
     assert.strictEqual(bytes(false), null);
     assert.strictEqual(bytes(NaN), null);
-    assert.strictEqual(bytes(function(){}), null);
+    assert.strictEqual(bytes(function () { }), null);
     assert.strictEqual(bytes({}), null);
   });
 
-  it('Shoud be able to parse a string into a number', function(){
-    // This function is testes more accurately in another test suite
+  it('Should be able to parse a string into a number', function () {
+    // This function is tested more accurately in another test suite
     assert.equal(bytes('1KB'), 1024);
   });
 
-  it('Should convert a number into a string', function(){
-    // This function is testes more accurately in another test suite
+  it('Should convert a number into a string', function () {
+    // This function is tested more accurately in another test suite
     assert.equal(bytes(1024), '1KB');
   });
 
-  it('Should convert a number into a string with options', function(){
-    // This function is testes more accurately in another test suite
-    assert.equal(bytes(1000, {thousandsSeparator: ' '}), '1 000B');
+  it('Should convert a number into a string with options', function () {
+    // This function is tested more accurately in another test suite
+    assert.equal(bytes(1000, { thousandsSeparator: ' ' }), '1 000B');
   });
 });
